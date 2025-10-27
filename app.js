4ff5e81375fbe43789457c67e935c575f9c08492
sroej <denkidev4@gmail.com>
2025-10-27 07:44:38 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+{
+    "name": "yoann session",
+    "description": "a session generator for yoann xmd-bot",
+    "logo": "https://files.catbox.moe/mwmtbh.jpg",
+    "repository": "https://github.com/denki-arch/YOANN-XMD",
+    "keywords": ["YOANN-XMD BOT"],
+    "success_url": "/", 
+    "buildpacks": [{ "url": "https://github.com/heroku/heroku-buildpack-nodejs#latest" } ],
+    "env": {      
+      "PORT": {
+        "description": "Port for web app.4000,5000,3000... any!",
+        "value": "5000",
+        "required": false 
+      },
+      "MESSAGE": {
+        "description": "This site is only for session generating",
+        "value": "",
+        "required": false 
+      }
+    }   
+
+}
